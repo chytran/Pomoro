2d4b8b5cc0d7097dfd043f01b1361a79a6a5b089
Kevin Tran <kevintran1142@gmail.com>
2021-12-24 11:57:40 -0800

    Imported home to pomoro

---
+++ public/assets/pomoro/javascript/main.js
@@ -0,0 +1,18 @@
+const navMenu = document.getElementById("nav-menu");
+const navToggle = document.getElementById("nav-toggle");
+const navClose = document.getElementById("nav-close");
+const navToggleBtn = document.getElementById("nav-toggle-button");
+// Show menu
+
+navToggle.addEventListener("click", () => {
+    navMenu.classList.add('show-menu');
+    navToggleBtn.classList.add('release');
+});
+
+
+if(navClose) {
+    navClose.addEventListener('click', () => {
+        navMenu.classList.remove('show-menu');
+        navToggleBtn.classList.remove('release');
+    })
+}
