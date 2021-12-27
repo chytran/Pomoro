78ad19479f810b09876b2b502cdca59ee3d8b9b2
Kevin Tran <kevintran1142@gmail.com>
2021-12-27 12:22:23 -0800

    Completed login, sign up, and check login function

--- app/models/user.php
+++ app/models/user.php
@@ -31,11 +31,32 @@ Class User
 
     function signup($POST)
     {
+        $DB = new Database();
 
+        $_SESSION['error'] = "";
+        if(isset($POST['username']) && isset($POST['password'])) 
+        {
+                // array for holding exact values
+            $arr['username'] = $POST['username']; // run if value is in array and isset
+            $arr['password'] = $POST['password'];
+            $arr['email'] = $POST['email'];
+            $query = "INSERT INTO users (username, password, email) values (:username, :password, :email)";
+            $data = $DB->write($query,$arr);
+            if($data) 
+            {
+                header("Location:" . ROOT . "login");
+                die;
+            } 
+        } else {
+            $_SESSION['error'] = 'Please enter a valid username and password';
+        }
+        
     }
 
     function check_logged_in()
     {
+        $DB = new Database();
+
         $arr['user_url'] = $POST['user_url'];
         if(isset($_SESSION['user_url'])) 
         {
@@ -47,7 +68,10 @@ Class User
                 $_SESSION['user_id'] = $data[0]->userid;
                 $_SESSION['user_name'] = $data[0]->username;
                 $_SESSION['user_url'] = $data[0]->url_address;
+
+                return true;
             }
         }
+        return false;
     }
 }
