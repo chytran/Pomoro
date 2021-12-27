c7d7a21c5d870d55e09cf5ea5a2bade5fac7feda
Kevin Tran <kevintran1142@gmail.com>
2021-12-27 12:16:42 -0800

    Checking log in function

--- app/models/user.php
+++ app/models/user.php
@@ -2,19 +2,52 @@
 
 Class User
 {
-    function login()
+    function login($POST)
     {
         $DB = new Database();
 
-        if(isset($POST['username']) && isset($POST['password']))
-        $query = "SELECT * FROM users where username = :username && password = :password limit 1";
-        $DB->write($query,$arr);
+        $_SESSION['error'] = "";
+        if(isset($POST['username']) && isset($POST['password'])) 
+        {
+                // array for holding exact values
+            $arr['username'] = $POST['username']; // run if value is in array and isset
+            $arr['password'] = $POST['password'];
+            $query = "SELECT * FROM users where username = :username && password = :password limit 1";
+            $data = $DB->read($query,$arr);
+            if(is_array($data)) 
+            {
+                // logged in
+                $_SESSION['user_id'] = $data[0]->userid;
+                $_SESSION['user_name'] = $data[0]->username;
+                $_SESSION['user_url'] = $data[0]->url_address;
+            } else {
+                $_SESSION['error'] = 'Wrong username and password';
+            }
+        } else {
+            $_SESSION['error'] = 'Please enter a valid username and password';
+        }
+        
     }
 
-    function signup()
+    function signup($POST)
     {
 
     }
 
-
+    function check_logged_in()
+    {
+        $arr['user_url'] = $POST['user_url'];
+        if(isset($_SESSION['user_url'])) 
+        {
+            $query = "SELECT * FROM users where url_address = :url_url limit 1";
+            $data = $DB->read($query,$arr);
+            if(is_array($data)) 
+            {
+                // logged in
+                $_SESSION['user_id'] = $data[0]->userid;
+                $_SESSION['user_name'] = $data[0]->username;
+                $_SESSION['user_url'] = $data[0]->url_address;
+            }
+        }
+    }
 }
