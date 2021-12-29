e56ddc84c321e5b9b74bc2d95b2fd41b7769b839
Kevin Tran <kevintran1142@gmail.com>
2021-12-29 12:55:54 -0800

    Letting signup controller have code as login

--- app/controllers/signup.php
+++ app/controllers/signup.php
@@ -4,10 +4,17 @@ Class Signup extends Controller
 {
     function index()
     {
-        $DB = new Database();
-        $data = $DB->read("SELECT * FROM USERS"); // data holds everything from query
         $data['title_page'] = 'Pomoro - Signup';
         
+        if(isset($_POST['email']))
+        {
+            $user = $this->loadModel("user");
+            $user->signup($_POST);
+        } else if(isset($_POST['username']) && !isset($_POST['email'])) {
+            $user = $this->loadModel("user");
+            $user->login($_POST);
+        }
+
         $this->view("signup", $data);
     }
 }
