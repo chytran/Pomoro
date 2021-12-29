6f1d7c45042dc66aebc315eb796a6874cda7ce5f
Kevin Tran <kevintran1142@gmail.com>
2021-12-29 12:52:01 -0800

    Editting login controller for login acceptance

--- app/controllers/login.php
+++ app/controllers/login.php
@@ -4,10 +4,19 @@ Class Login extends Controller
 {
     function index()
     {
-        $DB = new Database();
-        $data = $DB->read("SELECT * FROM USERS"); // data holds everything from query
+        // $DB = new Database();
+        // $data = $DB->read("SELECT * FROM USERS"); // data holds everything from query
         $data['title_page'] = 'Pomoro - Login';
         
+        if(isset($_POST['email']))
+        {
+            $user = $this->loadModel("user");
+            $user->signup($_POST);
+        } else if(isset($_POST['username']) && !isset($_POST['email'])) {
+            $user = $this->loadModel("user");
+            $user->login($_POST);
+        }
+
         $this->view("login", $data);
     }
 }
