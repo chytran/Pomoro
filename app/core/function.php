b45360ddafda651c4ce0c8a6063a674370bdb695
Kevin Tran <kevintran1142@gmail.com>
2021-12-31 11:59:20 -0800

    Adding uppercase alphabet to random function

--- app/core/function.php
+++ app/core/function.php
@@ -9,7 +9,7 @@ function show($stuff)
 
 function get_random_string_max($length) 
 {
-    $array = array(0,1,2,3,4,5,6,7,8,9,'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z');
+    $array = array(0,1,2,3,4,5,6,7,8,9,'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z');
     $text = '';
 
     $length = rand(4,$length);
