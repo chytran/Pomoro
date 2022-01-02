b5a0190d9d9d07f19348fe8894dc6655a3551d94
Kevin Tran <kevintran1142@gmail.com>
2022-01-01 20:44:10 -0800

    Aligning text center

--- app/views/error.php
+++ app/views/error.php
@@ -89,9 +89,9 @@
         <script src="<?=ASSETS ?>"></script>
 
 		<div id="left" class="grid place-items-center h-screen absolute w-full">
-                    <div class="bg-white relative w-6/12 h-3/6 object-cover">
-                        <div class="text-red-500 text-6xl left-44 absolute">Oops! Page not found</div>
-						<div class="text-red-500 text-2xl top-24 left-4 absolute text-center">The page you're looking for doesn't exist, or an error has occurred. Go back, or try refreshing your page.</div>
+                    <div class="bg-white relative w-full h-3/6 object-cover">
+                        <div class="text-4xl text-red-500 sm:w-full sm:text-6xl sm:left-22 absolute text-center md:-t-10">Oops! Page not found</div>
+						<div class="text-red-500 text-2xl top-24 left-4 w-full absolute text-center">The page you're looking for doesn't exist, or an error has occurred. Go back, or try refreshing your page.</div>
                     </div>
                 </div>
 
