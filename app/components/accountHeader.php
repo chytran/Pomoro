ae3ea9af88a596f4603064eecce86f9997715292
Kevin Tran <kevintran1142@gmail.com>
2022-01-06 12:24:11 -0800

    Setting up multiple interaction for account

---
+++ app/components/accountHeader.php
@@ -0,0 +1,16 @@
+<div id="container-account" class="w-full h-screen relative z-10">
+    <div id="adjustment" class="flex flex-row w-full h-screen">
+        <div id="left" class="pl-6 sm:pl-0 w-64 sm:w-72 md:w-96 lg:w-96  h-screen bg-red-700 z-10">
+            <div id="text-container" class="relative h-3/5 mt-24 pl-2 font-bold flex flex-col">
+                <a href="" class="text-white pt-6 sm:l hover:text-green-700 transition duration-200 sm:pl-10 md:text-xl">Dashboard</h1>
+                <a href="" class="text-white pt-6 sm:l hover:text-green-700 transition duration-200 sm:pl-10 md:text-xl">Cards</a>
+                <a href="" class="text-white pt-6 sm:l hover:text-green-700 transition duration-200 sm:pl-10 md:text-xl">Deposits/Withdraw</a>
+                <a href="" class="text-white pt-6 sm:l hover:text-green-700 transition duration-200 sm:pl-10 md:text-xl">History</h1>
+                <a href="" class="text-white pt-6 sm:l hover:text-green-700 transition duration-200 sm:pl-10 md:text-xl">Fund Transfer</a>
+            </div>
+        </div>
+        <div id="right" class="w-4/6 sm:w-4/6 md:w-4/6 h-screen bg-white z-10">
+
+        </div>  
+    </div>
+</div>
