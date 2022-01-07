840b26462f428f6b5e8d774eb1c6e69e5b8ecd03
Matt Razo <MatthewRazo16@gmail.com>
2022-01-06 20:27:08 -0800

    Base design for footer

--- app/components/footer.php
+++ app/components/footer.php
@@ -1,2 +1,97 @@
-</body>
-</html>
+
+    <title><?= $data['title_page'] ?></title>
+    <!--============ CSS ============-->
+    <link rel="stylesheet" href="<?=ASSETS?>pomoro/css/output.css">
+
+    <!--============ POPPINS ============-->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet">
+
+    <!--============ FAVICON ============-->
+    <link rel="icon" href="ASSETS?>pomoro/img/favicon.png">
+
+    <!--=============== BOXICONS ===============-->
+    <link rel="stylesheet" href="https://unpkg.com/boxicons@latest/css/boxicons.min.css">
+</head>
+<style>
+    *{
+        box-sizing: border-box;
+        padding: 0;
+        margin: 0;
+    }
+
+    html {
+        scroll-behavior: smooth;
+    }
+
+    body {
+        margin: 10px 0 0 0;
+    }
+
+    .show-menu {
+        top: 0;
+    }
+    .release {
+        color: transparent;
+        transition: 0.4s;
+    }
+    .transitioning {
+        transition: width 2s, height 2s, transform 2s;
+    }
+</style>
+<body>
+<!--==================== HEADER  ====================-->
+<footer id="footer" class="relative">
+    <div class="text-white text-transparent text-2xl md:text-3xl lg:text-4xl font-bold p-4 bg-red-700" style="font-family: poppins, sans-serif;">
+        <h1 class="text-white text-center"></h1>
+            <ul id="footer-utilities" class="float-left pr-10">
+                <h3 class="text-xl text-white ">Sign in</h3>
+                <h3 class="text-xl text-white ">Contact us</h3>
+                <h3 class="text-xl text-white ">Location finder</h3>
+                <h3 class="text-xl text-white ">Help</h3>
+            </ul>
+            <ul id="footer-links" class="float-left pr-10">
+                <h3 class="text-xl text-white ">Our Company</h3>
+                <h3 class="text-base text-white ">Responsible growth</h3>
+                <h3 class="text-base text-white ">Business Practices</h3>
+                <h3 class="text-base text-white ">What we offer</h3>
+            </ul>
+            <ul id="footer-links" class="float-left pr-10">
+                <h3 class="text-xl text-white ">Making an impact</h3>
+                <h3 class="text-base text-white ">Driving economic & social progress</h3>
+                <h3 class="text-base text-white ">Environmental sustainability</h3>
+                <h3 class="text-base text-white ">Find resources</h3>
+                <h3 class="text-base text-white ">Local impact</h3>
+            </ul>
+            <ul id="footer-links" class="float-left pr-10">
+                <h3 class="text-xl text-white ">Working here</h3>
+                <h3 class="text-base text-white ">Being a great place to work</h3>
+                <h3 class="text-base text-white ">Careers</h3>
+                <h3 class="text-base text-transparent ">test</h3>
+            </ul>
+            <ul id="footer-links" class="float-left pr-10">
+                <h3 class="text-xl text-white ">Investors</h3>
+                <h3 class="text-base text-white ">Profile</h3>
+                <h3 class="text-base text-white ">Quarterly earnings</h3>
+                <h3 class="text-base text-white ">Events & presentations</h3>
+                <h3 class="text-base text-white ">Regulatory & other filings</h3>
+                <h3 class="text-base text-white ">Fixed income</h3>
+                <h3 class="text-base text-white ">Shareholder info</h3>
+                <h3 class="text-base text-white ">Annual reports & proxy</h3>
+                <h3 class="text-base text-white ">Governance</h3>
+            </ul>
+            <ul id="footer-links" class="">
+                <h3 class="text-xl text-white ">Newsroom</h3>
+                <h3 class="text-base text-white ">Press releases</h3>
+                <h3 class="text-base text-white ">Executive biographies</h3>
+                <h3 class="text-base text-white ">Journalist resources</h3>
+                <h3 class="text-base text-white ">Awards & recognition</h3>
+                <h3 class="text-base text-transparent ">test</h3>
+                <h3 class="text-base text-transparent ">test</h3>
+                <h3 class="text-base text-transparent ">test</h3>
+                <h3 class="text-base text-transparent ">test</h3>
+            </ul>
+
+    </div>
+</footer>
