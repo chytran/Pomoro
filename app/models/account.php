68cf9bd3942f4ec330dbd309cfcd698039979ca9
Kevin Tran <kevintran1142@gmail.com>
2022-01-13 12:01:15 -0800

    importing home template

--- app/models/account.php
+++ app/models/account.php
@@ -2,5 +2,17 @@
 
 Class Account
 {
-    
+    function registerCard($POST) 
+    {
+        $DB = new Database();
+
+        $_SESSION['error'] = "";
+        if(isset($POST['username']))
+        {
+            // Array for holding exact values
+            $arr['username'] = $POST['username']; // run if value is in array and isset
+            $arr['email'] = $POST['email'];
+            $arr[];
+        }
+    }
 }
