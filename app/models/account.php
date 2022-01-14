3161ee88334ce6483ebdc7469c61a1c7faf411e5
Kevin Tran <kevintran1142@gmail.com>
2022-01-14 12:15:30 -0800

    Writing a function to make credit cards

--- app/models/account.php
+++ app/models/account.php
@@ -7,12 +7,22 @@ Class Account
         $DB = new Database();
 
         $_SESSION['error'] = "";
-        if(isset($POST['username']))
+        if(isset($POST['email']))
         {
             // Array for holding exact values
-            $arr['username'] = $POST['username']; // run if value is in array and isset
-            $arr['email'] = $POST['email'];
-            $arr[];
+            $arr['email'] = $POST['email']; // run if value is in array and isset
+            $arr['amount'] = 0;
+            $arr['creditCard'] = $POST['creditCard'];
+
+            $query = "INSERT INTO account (email, amount, creditCard) values (:email, :amount, :creditCard)";
+            $data = $DB->write($query, $arr);
+            if($data) 
+            {
+                header("Location:" . ROOT . "account/card");
+                die;
+            } else {
+                $_SESSION['error'] = 'Please enter valid information to create an card';
+            }
         }
     }
 }
