725a54cb7b843ff149da8810aa9a637f5cfef836
Kevin Tran <kevintran1142@gmail.com>
2022-01-16 11:07:47 -0800

    Fixing side header

--- app/components/accountHeader.php
+++ app/components/accountHeader.php
@@ -3,7 +3,7 @@
         <div id="left" style="transition: 0.3s;" class="z-20 absolute md:ml-0 transition flex flex-row justify-center items-center pl-6 sm:pl-0 w-56 sm:w-72 md:w-96 lg:w-96 h-screen bg-red-700 ">
             <div id="text-container" class="relative h-3/5 mt-24 font-bold flex flex-col">
                 <div id="link-container" class="relative w-11/12 rounded-xl hover:bg-red-100 transition duration-200 ease-in h-12 text-center flex flex-row justify-center md:ml-4">
-                    <a href="" class="text-white pt-3 pr-2 sm:l w-11/12 rounded-xl relative  hover:text-red-700 transition duration-200 sm:pl-10 md:text-xl md:pr-10">Dashboard</h1>
+                    <a href="dashboard" class="text-white pt-3 pr-2 sm:l w-11/12 rounded-xl relative  hover:text-red-700 transition duration-200 sm:pl-10 md:text-xl md:pr-10">Dashboard</h1>
                 </div>
                 <div class="relative w-11/12 rounded-xl hover:bg-red-100 transition duration-200 ease-in h-12 text-center flex flex-row justify-center -mt-3 md:ml-4">
                     <a href="" style="padding-right: 4.5rem;" class="text-white pt-3 pr-16 sm:l w-11/12 rounded-xl relative  hover:text-red-700 transition duration-200 sm:pl-10 md:text-xl md:pl-0 md:mr-16">Cards</a>
