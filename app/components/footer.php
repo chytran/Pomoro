30bb8d73729a59f66c573f8d2abc24803555d3bc
Matt Razo <MatthewRazo16@gmail.com>
2022-01-17 10:24:30 -0800

    Footer remade. Account history mobile view made

--- app/components/footer.php
+++ app/components/footer.php
@@ -1,50 +1,50 @@
 
 <!--==================== FOOTER ====================-->
     <footer id="footer" style="" class="relative sm:h-96;">
-        <div class="text-white text-transparent text-2xl md:text-3xl lg:text-3xl font-bold p-4 bg-red-700 h-full" style="font-family: poppins, sans-serif;">
-            <!-- <h1 class="text-white text-center"></h1> -->
-            <ul id="footer-utilities" class="float-left pr-10">
-                <h3 class="text-xl text-white ">Sign in</h3>
-                <h3 class="text-xl text-white ">Contact us</h3>
-                <h3 class="text-xl text-white ">Location finder</h3>
-                <h3 class="text-xl text-white ">Help</h3>
-            </ul>
-            <ul id="footer-links" class="float-left pr-10">
-                <h3 class="text-xl text-white ">Our Company</h3>
-                <h3 class="text-base text-white ">Responsible growth</h3>
-                <h3 class="text-base text-white ">Business Practices</h3>
-                <h3 class="text-base text-white ">What we offer</h3>
-            </ul>
-            <ul id="footer-links" class="float-left pr-10">
-                <h3 class="text-xl text-white ">Making an impact</h3>
-                <h3 class="text-base text-white ">Driving economic & social progress</h3>
-                <h3 class="text-base text-white ">Environmental sustainability</h3>
-                <h3 class="text-base text-white ">Find resources</h3>
-                <h3 class="text-base text-white ">Local impact</h3>
-            </ul>
-            <ul id="footer-links" class="float-left pr-10">
-                <h3 class="text-xl text-white ">Working here</h3>
-                <h3 class="text-base text-white ">Being a great place to work</h3>
-                <h3 class="text-base text-white ">Careers</h3>
-            </ul>
-            <ul id="footer-links" class="float-left pr-10">
-                <h3 class="text-xl text-white ">Investors</h3>
-                <h3 class="text-base text-white ">Profile</h3>
-                <h3 class="text-base text-white ">Quarterly earnings</h3>
-                <h3 class="text-base text-white ">Events & presentations</h3>
-                <h3 class="text-base text-white ">Regulatory & other filings</h3>
-                <h3 class="text-base text-white ">Fixed income</h3>
-                <h3 class="text-base text-white ">Shareholder info</h3>
-                <h3 class="text-base text-white ">Annual reports & proxy</h3>
-                <h3 class="text-base text-white ">Governance</h3>
-            </ul>
-            <ul id="footer-links" class="">
-                <h3 class="text-xl text-white ">Newsroom</h3>
-                <h3 class="text-base text-white ">Press releases</h3>
-                <h3 class="text-base text-white ">Executive biographies</h3>
-                <h3 class="text-base text-white ">Journalist resources</h3>
-                <h3 class="text-base text-white ">Awards & recognition</h3>
-            </ul>
+        <div class="text-white text-transparent text-2xl md:text-3xl lg:text-3xl p-4 bg-red-700 h-full" style="font-family: poppins, sans-serif;">
+            <div class="grid sm:grid-flow-col gap-2 sm:auto-cols-auto">
+                <div class="columns-xs">
+                    <h1 class="font-bold p-1"><a href="">Sign In</a></h1>
+                    <h1 class="font-bold p-1"><a href="">Contact us</a></h1>
+                    <h1 class="font-bold p-1"><a href="">Location finder</a></h1>
+                    <h1 class="font-bold p-1"><a href="">Help</a></h1>
+                </div>
+                <div class="columns-xs">
+                    <h1 class="font-bold">Our Company</h1>
+                    <h2 class="text-base"><a href="">Responsible growth</a></h2>
+                    <h2 class="text-base"><a href="">Business PRactices</a></h2>
+                    <h2 class="text-base"><a href="">What we offer</a></h2>
+                </div>
+                <div class="columns-xs">
+                    <h1 class="font-bold">Making an impact</h1>
+                    <h2 class="text-base"><a href="">Driving econmic & social progress</a></h2>
+                    <h2 class="text-base"><a href="">Environmental sustainability</a></h2>
+                    <h2 class="text-base"><a href="">Find resources</a></h2>
+                    <h2 class="text-base"><a href="">Local impact</a></h2>
+                </div>
+                <div class="columns-xs">
+                    <h1 class="font-bold">Working here</h1>
+                    <h2 class="text-base"><a href="">Being a great place to work</a></h2>
+                    <h2 class="text-base"><a href="">Careers</a></h2>
+                </div>
+                <div class="columns-xs">
+                    <h1 class="font-bold">Investors</h1>
+                    <h2 class="text-base"><a href="">Profile</a></h2>
+                    <h2 class="text-base"><a href="">Quarterly earnings</a></h1>
+                    <h2 class="text-base"><a href="">Events & presentations</a></h2>
+                    <h2 class="text-base"><a href="">Fixed income</a></h2>
+                    <h2 class="text-base"><a href="">Shareholder info</a></h2>
+                    <h2 class="text-base"><a href="">Annual reports & proxy</a></h2>
+                    <h2 class="text-base"><a href="">Governance</a></h2>
+                </div>
+                <div class="columns-xs">
+                    <h1 class="font-bold">Newsrooms</h1>
+                    <h2 class="text-base"><a href="">Press releases</a></h2>
+                    <h2 class="text-base"><a href="">Executive biographies</a></h2>
+                    <h2 class="text-base"><a href="">Journalist resources</a></h2>
+                    <h2 class="text-base"><a href="">Awards & recognition</a></h2>
+                </div>
+            </div>
         </div>
     </footer>
 </body>
