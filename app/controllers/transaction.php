9b2843e7d3f2ae4c28974502a81a043ccfa043c8
Kevin Tran <kevintran1142@gmail.com>
2022-01-24 12:55:54 -0800

    Display all options for which card to choose

--- app/controllers/transaction.php
+++ app/controllers/transaction.php
@@ -4,9 +4,13 @@ class Transaction extends Controller
 {
     function index()
     {
-        $DB = new Database();
-        $data = $DB->read("SELECT * FROM USERS"); // data holds everything from query
         $data['title_page'] = 'Pomoro - Transaction';
+
+        $account = $this->loadModel("account");
+        $accountTime = new Account;
+        $result = $accountTime->get_all();
+
+        $data['cards'] = $result;
         
         $this->view("accountTransaction", $data);
     }
--- app/views/accountTransaction.php
+++ app/views/accountTransaction.php
@@ -12,7 +12,26 @@
 ?>
 
 <!-- Right side of account -->
-
+<div id="right" class="w-full sm:w-full md:w-full h-screen bg-white z-10 pt-14">
+    <div id="organizer-adjustment" class="bg-white relative z-20 flex flex-col justify-center h-screen w-full">
+        <div id="outer-border" class="w-full h-screen bg-red-100 flex flex-col justify-center items-center">
+            <!-- Which card -->
+            <div class="which__card__container">
+                <select required name="creditCard" id="creditCardSelect" class="bg-transparent border-b-2 cursor-pointer hover:border-b-8 w-full outline-none pb-2" select>
+                    <option value>-- --</option>
+                    <?php if(is_array($data['cards'])): ?>
+                        <?php foreach($data['cards'] as $row):?>
+                            <option value="<?=$row->creditCard?>"><?=$row->creditCard?> Card</option>
+                        <?php endforeach;?>
+                    <?php endif; ?>
+                </select>
+            </div>
+            
+            <!-- Deposit or Withdraw  -->
+            <!-- Submit -->
+        </div>
+    </div>
+</div>
 
 <!-- End of accountheader -->
 <?php
