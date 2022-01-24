5fda2d6e1097a8219efd23df5f5b1b498e7b0872
Kevin Tran <kevintran1142@gmail.com>
2022-01-24 13:21:27 -0800

    setting up transaction function in controller

--- app/controllers/transaction.php
+++ app/controllers/transaction.php
@@ -10,7 +10,24 @@ class Transaction extends Controller
         $accountTime = new Account;
         $result = $accountTime->get_all();
 
+        // Data for cards
         $data['cards'] = $result;
+
+        // isset then add
+        if(isset($_POST['transactionTime']) && isset($_POST['amount']) && isset($_POST['card'])) {
+            if($_POST['transactionTime'] = 'withdraw') {
+                $withdraw = $this->loadModel("account");
+                $withdrawTime = new Account;
+                $withdrawTime->withdrawNumbers($_POST);
+            } else {
+                $deposit = $this->loadModel("account");
+                $depositTime = new Account;
+                $depositTime->depositNumbers($_POST);
+            }
+        } else {
+            // Error if necessary
+        }
+        // else subtract
         
         $this->view("accountTransaction", $data);
     }
