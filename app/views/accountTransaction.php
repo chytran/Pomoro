7f02c9aedb0886e9b67595dc4b41aeb00abb6ace
Kevin Tran <kevintran1142@gmail.com>
2022-01-27 12:45:17 -0800

    Continue to adjust design

--- app/views/accountTransaction.php
+++ app/views/accountTransaction.php
@@ -19,7 +19,7 @@
                 <form method="POST" class="w-full flex flex-col gap-7 justify-center items-center">
                     <!-- Which card -->
                     <div class="which__card__container w-4/6 text-center flex mr-3">
-                        <span style="font-family: Poppins, sans-serif;" class="border-b-2 cursor-pointer w-full font-medium -ml-4 text-sm sm:text-xs sm:w-4/6 md:text-sm md:w-2/6 md:-ml-2 lg:w-1/6 lg:-ml-1 lg:text-xs pt-1" required>SELECT CARD:</span>
+                        <span style="font-family: Poppins, sans-serif;" class="border-b-2 cursor-pointer w-full font-medium -ml-4 text-sm sm:text-sm sm:w-4/6 md:text-sm md:w-4/6 md:-ml-2 lg:w-3/6 lg:-ml-1 lg:text-sm pt-1" required>SELECT CARD:</span>
                         <select required name="creditCardChange" id="creditCardSelect" class="bg-transparent border-b-2 cursor-pointer hover:border-b-8 w-full outline-none pb-2" select>
                             <option value>-- --</option>
                             <?php if(is_array($data['cards'])): ?>
@@ -30,12 +30,12 @@
                         </select>
                     </div>
                     <div id="email" class="w-4/6 text-center flex mr-3">
-                        <span style="font-family: Poppins, sans-serif;" class="border-b-2 cursor-pointer w-full font-medium -ml-4 text-sm sm:text-xs sm:w-4/6 md:text-sm md:w-2/6 md:-ml-2 lg:w-1/6 lg:-ml-1 lg:text-xs pt-1" required>CONFIRM EMAIL:</span>
+                        <span style="font-family: Poppins, sans-serif;" class="border-b-2 cursor-pointer w-full font-medium -ml-4 text-sm sm:text-sm sm:w-full md:text-sm md:w-5/6 md:-ml-2 lg:w-3/6 lg:-ml-1 lg:text-sm pt-1" required>CONFIRM EMAIL:</span>
                         <input name="email2" type="text" class="pl-8 bg-transparent border-b-2 cursor-pointer w-full outline-none pb-2" required>
                     </div>
                     <!-- Deposit or Withdraw  -->
                     <div class="deposit__withdraw__container w-4/6 text-center flex mr-3">
-                        <span style="font-family: Poppins, sans-serif;" class="border-b-2 w-full cursor-pointer text-sm font-medium -ml-4 sm:text-xs sm:w-4/6 md:text-sm md:w-2/6 md:-ml-2 lg:w-1/6 lg:-ml-1 lg:text-xs pt-1" required>TRANSACTION TYPE:</span>
+                        <span style="font-family: Poppins, sans-serif; width: 110%;" class="border-b-2 cursor-pointer text-sm font-medium -ml-4 sm:text-sm sm:w-full md:text-sm md:w-5/6 md:-ml-2 lg:w-3/6 lg:-ml-1 lg:text-sm pt-1" required>TRANSACTION TYPE:</span>
                         <select required name="depositOrWithdraw" class="bg-transparent border-b-2 cursor-pointer hover:border-b-8 w-full outline-none pb-2" select>
                             <option value>-- --</option>
                             <option class="text-xs" value="withdraw">Withdraw</option>
@@ -45,7 +45,7 @@
                     
                     <!-- How much -->
                     <div id="amountChange" class="w-4/6 text-center flex mr-3">
-                        <span style="font-family: Poppins, sans-serif;" class="-ml-4 border-b-2 cursor-pointer w-full font-medium sm:ml-2 text-sm sm:text-xs sm:w-4/6 md:text-sm md:w-2/6 md:-ml-2 lg:w-2/6 lg:-ml-12 lg:text-xs">AMOUNT: </span>
+                        <span style="font-family: Poppins, sans-serif;" class="-ml-4 border-b-2 cursor-pointer w-full font-medium sm:ml-2 sm:pr-20 text-sm sm:text-sm sm:w-4/6 md:text-sm md:w-2/6 md:-ml-2 lg:w-3/6 lg:pr-14 lg:text-sm">AMOUNT: </span>
                         <input required name="amountChange" type="text" class="pl-8 bg-transparent border-b-2 cursor-pointer hover:border-b-8 w-full outline-none">
                     </div>
                     <!-- Submit -->
