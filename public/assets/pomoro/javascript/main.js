e1b2af0a12ba6b393291c31d12429c02a998dae8
Kevin Tran <kevintran1142@gmail.com>
2022-01-30 13:40:46 -0800

    adding typescript

--- public/assets/pomoro/javascript/main.js
+++ public/assets/pomoro/javascript/main.js
@@ -1,20 +1,17 @@
-const navMenu = document.getElementById("nav-menu"); // menu
-const navToggle = document.getElementById("nav-toggle"); // toggle
-const navClose = document.getElementById("nav-close");
-const navToggleBtn = document.getElementById("nav-toggle-button");
+"use strict";
+exports.__esModule = true;
+var navMenu = document.getElementById("nav-menu"); // menu
+var navToggle = document.getElementById("nav-toggle"); // toggle
+var navClose = document.getElementById("nav-close");
+var navToggleBtn = document.getElementById("nav-toggle-button");
 // Show menu
-
-navToggle.addEventListener("click", () => {
+navToggle.addEventListener("click", function () {
     navMenu.classList.add('show-menu');
     navToggleBtn.classList.add('release');
 });
-
-
-if(navClose) {
-    navClose.addEventListener('click', () => {
+if (navClose) {
+    navClose.addEventListener('click', function () {
         navMenu.classList.remove('show-menu');
         navToggleBtn.classList.remove('release');
-    })
+    });
 }
-
-[]
---
+++ public/assets/pomoro/javascript/main.ts
@@ -0,0 +1,20 @@
+export {};
+const navMenu = document.getElementById("nav-menu"); // menu
+const navToggle = document.getElementById("nav-toggle"); // toggle
+const navClose = document.getElementById("nav-close");
+const navToggleBtn = document.getElementById("nav-toggle-button");
+// Show menu
+
+navToggle.addEventListener("click", () => {
+    navMenu.classList.add('show-menu');
+    navToggleBtn.classList.add('release');
+});
+
+
+if(navClose) {
+    navClose.addEventListener('click', () => {
+        navMenu.classList.remove('show-menu');
+        navToggleBtn.classList.remove('release');
+    })
+}
+
