4f9395d058b4c8076978aed4684be8b3386a1ada
Kevin Tran <kevintran1142@gmail.com>
2022-02-03 20:43:24 -0800

    Debugging design

--- app/views/ecommerceHome.php
+++ app/views/ecommerceHome.php
@@ -11,9 +11,9 @@
             <div id="gridsetup" class="grid sm:grid-cols-2 gap-3 md:grid-cols-3 lg:grid-cols-4">
                 <?php if(is_array($data['products'])): ?>
                     <?php foreach($data['products'] as $row):?>
-                        <div class="rounded-xl justify-center items-start border-2 border-black bg-red-100 hover:bg-white transition duration-200 ease-in">
+                        <div class="flex flex-col justify-center items-center rounded-xl border-2 border-black bg-red-100 hover:bg-white transition duration-200 ease-in">
                             <h1 style="font-family: poppins, sans-serif; text-indent: 5px;" class="text-black font-semi-bold text-lg"><?=$row->name?></h1>
-                            <img class="object-cover" src="<?=ASSETS?>pomoro/img/product<?=$row->id?>.png" alt="image of <?=$row->name?>"/>
+                            <img class="object-cover w-64 h-64" src="<?=ASSETS?>pomoro/img/product<?=$row->id?>.png" alt="image of <?=$row->name?>"/>
                             <div style="font-family: poppins, sans-serif;" class="text-black pt-2 text-md " id="price">$<?=$row->price?></div>
                             <div style="font-family: poppins, sans-serif;" class="text-black pt-2 text-sm " id="description"><?=$row->description?></div>
                             <div class="text-white bg-red-500 hover:bg-red-800 transition duration-200 ease-in cursor-pointer w-full h-9 rounded-none mt-4 flex justify-center items-center" id="add-cart"><a href="#" class="text-sm">Add to Cart</a></div>
