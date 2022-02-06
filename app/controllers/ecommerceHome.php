d2f825dec6bcf0acdb55866d242518ee53eb2337
Kevin Tran <kevintran1142@gmail.com>
2022-02-06 10:56:36 -0800

    pushing code

--- app/controllers/ecommerceHome.php
+++ app/controllers/ecommerceHome.php
@@ -2,50 +2,50 @@
 
 Class EcommerceHome extends Controller
 {
-    function index($b, $c)
+    function index()
     {
-        if ($b == 'page') {
-            $data['title_page'] = 'Pomoro - Home';
-        
-            //fill data['product'] with product info
-            $product = $this->loadModel("product");
-            $productTime = new Product;
+        // if ($b == 'page') {
+        $data['title_page'] = 'Pomoro - Home';
+    
+        //fill data['product'] with product info
+        $product = $this->loadModel("product");
+        $productTime = new Product;
 
-            //allows calling total # of products
-            $numOfProducts = $productTime->getCountOfProducts();
-            $data['numOfProducts'] = $numOfProducts;
-            $productsPerPage = 8;
+        //allows calling total # of products
+        $numOfProducts = $productTime->getCountOfProducts();
+        $data['numOfProducts'] = $numOfProducts;
+        $productsPerPage = 8;
 
             // pagination
-            for ($i = 1; $i <= ($numOfProducts / $productsPerPage);  $i++) {
-                // $result temporary holds values to push to array
+            // for ($i = 1; $i <= ($numOfProducts / $productsPerPage);  $i++) {
+            //     // $result temporary holds values to push to array
                 
-                $result = $productTime->getProductsPerPage($i); // pushes data
+            //     $result = $productTime->getProductsPerPage($i); // pushes data
 
-                // Hold page numbers
-                array_push($data['pageNumber'][$i], $i);
+            //     // Hold page numbers
+            //     array_push($data['pageNumber'][$i], $i);
 
-                // Holds data results
-                array_push($data['products'][$i], $result);
+            //     // Holds data results
+            //     array_push($data['products'][$i], $result);
 
-            }
+            // }
 
-            // If url page # = #i 
-            // $c needs to be?
-            if (in_array($c, $data['pageNumber'])) {
-                $data['displayData'] = $data['products'][$c];
-                if (!$c < 2) {
-                    $data['pageNumberBack'] = $c - 1; // page number if user clicks left arrow
-                    $data['pageNumberForward'] = $c + 1; // page number if user clicks right arrow
-                }           
-            }
+            // // If url page # = #i 
+            // // $c needs to be?
+            // if (in_array($c, $data['pageNumber'])) {
+            //     $data['displayData'] = $data['products'][$c];
+            //     if (!$c < 2) {
+            //         $data['pageNumberBack'] = $c - 1; // page number if user clicks left arrow
+            //         $data['pageNumberForward'] = $c + 1; // page number if user clicks right arrow
+            //     }           
+            // }
 
-            // $data['products'] = $result;
-            //testing # of products per page and pagination
-            $data['productsPerPage'] = 8;
-            $data['pageOffset'] = 0;
-        }
-        
+            
+            // //testing # of products per page and pagination
+            // $data['productsPerPage'] = 8;
+            // $data['pageOffset'] = 0;
+        // }
+        $data['products'] = $result;
         $this->view("ecommerceHome", $data);
     }
 
