4f7c0901d1ea071b905737bde8f165b83ad4ca62
Kevin Tran <kevintran1142@gmail.com>
2022-02-06 10:39:04 -0800

    progress in pagination data pushing

--- app/controllers/ecommerceHome.php
+++ app/controllers/ecommerceHome.php
@@ -2,24 +2,37 @@
 
 Class EcommerceHome extends Controller
 {
-    function index()
+    function index($b, $c)
     {
-        $data['title_page'] = 'Pomoro - Home';
+        if ($b == 'page') {
+            $data['title_page'] = 'Pomoro - Home';
         
-        //fill data['product'] with product info
-        $product = $this->loadModel("product");
-        $productTime = new Product;
-        $result = $productTime->getProducts();
-        $data['products'] = $result;
+            //fill data['product'] with product info
+            $product = $this->loadModel("product");
+            $productTime = new Product;
 
-        //allows calling total # of products
-        $numOfProducts = $productTime->getCountOfProducts();
-        $data['numOfProducts'] = $numOfProducts;
+            //allows calling total # of products
+            $numOfProducts = $productTime->getCountOfProducts();
+            $data['numOfProducts'] = $numOfProducts;
+            $productsPerPage = 8;
 
-        //testing # of products per page and pagination
-        $data['productsPerPage'] = 8;
-        $data['pageOffset'] = 0;
+            // pagination
+            for ($i = 0; $i < ($numOfProducts / $productsPerPage);  $i++) {
+                $result = $productTime->getProductsPerPage($i);
+                array_push($data['products'][$i], $result);
+            }
 
+            // If url page # = #i 
+            // $c needs to be?
+
+            $data['products'] = $result;
+            
+
+            //testing # of products per page and pagination
+            $data['productsPerPage'] = 8;
+            $data['pageOffset'] = 0;
+        }
+        
         $this->view("ecommerceHome", $data);
     }
 
--- app/models/product.php
+++ app/models/product.php
@@ -30,4 +30,12 @@ Class Product
         $data = $DB->read($query);
         return intval($data);
     }
+
+    function getProductsPerPage($page) {
+        $sqlQuery = "SELECT * FROM products Limit 8, " + $page; 
+
+        $DB = new Database();
+        $data = $DB->read($query);
+        return intval($data);
+    }
 }
