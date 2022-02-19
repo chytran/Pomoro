1130f380d21d39b3f5d1102fc1c0f7ba30af0909
Kevin Tran <kevintran1142@gmail.com>
2022-02-18 22:57:48 -0800

    setting up foundation for 2nd home

--- app/views/homePage2.php
+++ app/views/homePage2.php
@@ -10,9 +10,16 @@
         <!-- Account -->
         <div id="account" class="h-screen relative m-auto w-screen bg-gradient-to-bl from-red-100 to-red-400 flex flex-column items-center justify-center bg-black"> -->
             <!-- <img id="credit-img" src="<?=ASSETS?>pomoro/img/credit.png" alt="credit-hand" class="invisible lg:visible lg:right-0 xl:right-44 absolute bottom-0"> -->
-            <!-- Left -->
+            
             <div id="text-container" class="w-4/5 h-4/5 flex flex-column items-center justify-center bg-black p-12">
-                
+                <!-- Left -->
+                <div id="ecommerce-home">
+                    
+                </div>
+                <!-- Right -->
+                <div id="bank-home">
+
+                </div>
             </div> 
         </div> 
         <!-- <div id="advertisement" class="h-full w-full bg-black">
