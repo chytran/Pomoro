74a18094d71baf8163b996416cca6df74218a638
Kevin Tran <kevintran1142@gmail.com>
2022-02-23 05:33:20 -0800

    testlogin function created

--- app/testing/test.php
+++ app/testing/test.php
@@ -1,3 +1,12 @@
 <?php
 
 // Testing for login confirmation
+Class Testing
+{
+
+    /*================== Register ==========================*/
+    function testLogin($POST) 
+    {
+        
+    }
+}
