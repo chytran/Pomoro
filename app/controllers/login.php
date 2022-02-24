7e137945358173a610e2f7f026029da8425ff83f
chytran <kevintran1142@gmail.com>
2022-02-23 20:56:44 -0800

    Loading test function for login

--- app/controllers/login.php
+++ app/controllers/login.php
@@ -11,10 +11,25 @@ Class Login extends Controller
         if(isset($_POST['email']))
         {
             $user = $this->loadModel("user");
+
+            // Test
+            $this->loadTest("test");
+            $testTime = new Testing;
+            $testTime->testLogin($_POST);
+
+            // User model call
             $userTime = new User;
             $userTime->signup($_POST);
         } else if(isset($_POST['username']) && !isset($_POST['email'])) {
+
             $user = $this->loadModel("user");
+
+            // Test
+            $this->loadTest("test");
+            $testTime = new Testing;
+            $testTime->testLogin($_POST);
+
+            // User model call
             $userTime = new User;
             $userTime->login($_POST);
         }
