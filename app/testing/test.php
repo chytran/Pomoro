12263b8d5e49336fad621323a86c0d41f3067fe4
chytran <kevintran1142@gmail.com>
2022-02-23 20:52:45 -0800

    login test function

--- app/testing/test.php
+++ app/testing/test.php
@@ -7,6 +7,15 @@ Class Testing
     /*================== Register ==========================*/
     function testLogin($POST) 
     {
-        
+        $DB = new Database();
+
+        $_SESSION['error'] = '';
+
+        if(isset($POST['username']) && isset($POST['password']))
+        {
+            print_r("Login Successful");
+        } else {
+            $_SESSION['error'] = "login test failed";
+        }
     }
 }
