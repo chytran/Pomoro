a11f3725d3dec45e61b98650fe9cdd753a6d0369
Kevin Tran <kevintran1142@gmail.com>
2022-02-25 04:15:22 -0800

    update sql queries

--- app/models/cart.php
+++ app/models/cart.php
@@ -2,11 +2,12 @@
 
 Class Cart
 {
+    /*xxxxxxxxxxxxxxx needs $POST to insert xxxxxxxxxxxxxxxx*/
     function addToCart($username, $productID) {
         $query = "INSERT INTO cart (username, productID) VALUES (" . $username . ", " . $productID . ");";
         
         $DB = new Database();
-        $data = $DB->read($query);
+        $data = $DB->write($query);
     }
     
     function getCartItems($username) {
@@ -25,13 +26,13 @@ Class Cart
         $query = "DELETE FROM cart WHERE username = " . $username . " AND productID = " . $productID . ";";
         
         $DB = new Database();
-        $data = $DB->read($query);
+        $data = $DB->write($query);
     }
 
     function emptyCart($username) {
-        $query = "DELETE FROM cart WHERE username = " . $username . ";";
+        $query = "DELETE * FROM cart WHERE username = " . $username . ";";
         
         $DB = new Database();
-        $data = $DB->read($query);
+        $data = $DB->write($query);
     }
 }
