5c354d1849fd5eef94e89fadedaa0f108d57ef94
nrmalone <nicholasrmalone99@gmail.com>
2022-03-02 19:29:19 -0800

    populate user's cart

--- app/views/shoppingCart.php
+++ app/views/shoppingCart.php
@@ -1,20 +1,37 @@
 <?php
     require_once '../app/components/htmlSetup.php';
 ?>
+<?php
+    require_once '../app/components/ecommerceHeader.php';
+?>
 <body>
-    <div id="main-container" class="flex justify-center items-center w-full h-screen">
+    <div id="main-container" class="mt-12 flex justify-center items-center w-screen h-full bg-gradient-to-t from-red-100 to-red-400">
         <div id="horizontal-container" class="px-24 py-8 w-full h-full flex flex-col justify-center items-center md:flex-row">
             <!-- 4/6 -->
-            <div id="left" class="h-full w-full md:w-4/6 pr-6 ">
+            <div id="left" class="h-full w-full md:w-4/6 pr-6">
                 <div id="vertical-container-left" class="flex flex-col justify-center items-start">
-                    <div id="free-shipping-container" class="border-2 w-full h-1/6">
-                        <h1 class="text-red-400 font-semibold">Free Shipping for Members.</h1>
-                        <span class="text-gray-500">Become a Nike Member for fast and free shipping. Join us or Sign-in</span>
-                    </div>
                     <div id="items-container" class="w-full flex flex-col justify-start items-start">
-                        <h1 class="font-bold">Bag</h1>
+                        <h1 class="text-2xl font-bold"><?php echo $_SESSION['user_name1'] ?>'s Cart</h1>
                         <!-- Copy Item -->
-                        <div id="individual-items-container" class="w-full h-3/6">
+                        <?php if ($data['cartItems'] != false) {
+                            foreach($data['cartItems'] as $row) {
+                                echo '<form method="POST">';
+                                echo '<div id="individual-items-container" class="w-full h-3/6"><div id="item" class="w-full flex flex-row justify-between items-center gap-2">';
+                                    echo '<img src="' . ASSETS . 'pomoro/img/product' . $row->productID . '.png" alt="" class="w-24 h-24"';
+                                    echo '<div id="item-detail-container" class="w-full">';
+                                        echo '<div id="title-price" class="flex flex-row justify-between items-start">';
+                                            echo '<h1 class="font-semibold w-1/2">' . $row->name . '</h1><h1>$' . $row->price . '</h1>';
+                                        echo '</div>';
+                                        echo '<span class="text-black w-1/2">' . $row->description . '</span>';
+                                        echo '<div id="favorite-remove" class="text-black"><input name="cartID" type="hidden" value="' . $row->id . '">';
+                                            echo '<button class="text-white bg-red-500 hover:bg-red-800 transition duration-200 ease-in cursor-pointer w-full h-9 rounded-full mx-1 flex justify-center items-center">Remove</button>';
+                                        echo '</div>';
+                                    echo '</div>';
+                                echo '</div></div>';
+                                echo '</form>';
+                            }
+                        } ?>
+                        <!-- <div id="individual-items-container" class="w-full h-3/6">
                             <div id="item" class="w-full flex flex-row justify-between items-center gap-2">
                                 <img src="<?=ASSETS?>pomoro/img/shoes.jpg" alt="" class="w-24 h-24 ">
                                 <div id="item-detail-container" class="w-full">
@@ -52,8 +69,10 @@
                                 </div>
                             </div>
                         </div>
+                        -->
                         <!-- Copy Item -->
-                        <div id="shipping-details" class="w-full h-2/6">
+                        
+                        <!-- <div id="shipping-details" class="w-full h-2/6">
                             <h1 class="text-black font-bold">Shipping</h1>
                             <h2 class="text-black font-semibold">Arrives Tue, Jul 20 - Thu, Jul 22</h2>
                             <h1 class="text-black font-bold">Pickup</h1>
@@ -63,6 +82,8 @@
                             <div id="icon-alert-items" class=""><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path fill="none" d="M0 0h24v24H0z"/><path d="M17.618 5.968l1.453-1.453 1.414 1.414-1.453 1.453a9 9 0 1 1-1.414-1.414zM12 20a7 7 0 1 0 0-14 7 7 0 0 0 0 14zM11 8h2v6h-2V8zM8 1h8v2H8V1z" fill="rgba(202,138,4,1)"/></svg></div>
                             <span class="">Just a few left. Order soon</span>
                         </div>
+                        -->
+
                     </div>
                 </div>
             </div>
@@ -84,7 +105,7 @@
                                     <div id="question-icon" class="pl-1"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="18" height="18"><path fill="none" d="M0 0h24v24H0z"/><path d="M12 22C6.477 22 2 17.523 2 12S6.477 2 12 2s10 4.477 10 10-4.477 10-10 10zm-1-7v2h2v-2h-2zm2-1.645A3.502 3.502 0 0 0 12 6.5a3.501 3.501 0 0 0-3.433 2.813l1.962.393A1.5 1.5 0 1 1 12 11.5a1 1 0 0 0-1 1V14h2v-.645z"/></svg></div>
                                 </div>
                                 <div id="subtotal-right-container" class="w-1/3">
-                                    <div id="subtotal-price" class="">$90.00</div>
+                                    <div id="subtotal-price" class="">$<?php $data['cartSubtotal'] ?></div>
                                 </div>
                                 
                             </div>
@@ -93,7 +114,7 @@
                                     <h3 class="text-black font-medium">Estimated Shipping & Handling</h3>
                                 </div>
                                 <div id="subtotal-right-container" class="w-1/3">
-                                    <div id="estimate-shipping-price" class="">$8.00</div>
+                                    <div id="estimate-shipping-price" class="">$</div>
                                 </div>
                             </div>
                             <div id="estimate-tax-container" class="flex flex-row justify-start items-start pt-3">
@@ -112,7 +133,7 @@
                             </div>
                             
                             <div id="total-container-right" class="w-1/3">
-                                <h1 class="">$98.00</h1>
+                                <h1 class="">$</h1>
                             </div>
                         </div> <!-- total-container end -->
 
@@ -127,3 +148,7 @@
     </div>
 </body>
 </html>
+
+<?php
+    include_once '../app/components/footer.php'
+?>
